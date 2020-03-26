db5acfb6c2d1ac03ffd3284f2515493d9207749d
WISUNGWON <tkrhkaos12@naver.com>
2020-03-26 18:53:38 +0900

    End Chapter3

---
+++ src/main/java/springboot/service/posts/PostsService.java
@@ -0,0 +1,48 @@
+package springboot.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import springboot.domain.posts.Posts;
+import springboot.domain.posts.PostsRepository;
+import springboot.web.dto.PostsResponseDto;
+import springboot.web.dto.PostsSaveRequestDto;
+import springboot.web.dto.PostsUpdateRequestDto;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("The user is not found. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("The user is not found. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+
+
+
+
+
+
+
+
+
+}
